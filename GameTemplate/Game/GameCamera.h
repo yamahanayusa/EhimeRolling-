a2cd30dfa03f1ce8b47549331d5fcfbf90c4a7e2
anoyasu <CA01244001@st.kawahara.ac.jp>
2025-03-04 14:39:07 +0900

    GameCamera01

--- GameTemplate/Game/GameCamera.h
+++ GameTemplate/Game/GameCamera.h
@@ -1,6 +1,29 @@
 #pragma once
-class GameCamera
+
+//ばねカメラを使用したい場合は、SpringCameraをインクルードする。
+#include "camera/SpringCamera.h"
+
+//クラス宣言
+class Player;
+
+class GameCamera:public IGameObject
 {
+public:
+
+	GameCamera();
+	~GameCamera();
+	//スタート
+	bool Start();
+	//アップデート
+	void Update();
+	//座標と視点を更新する
+	void UpdatePositionAndTarget();
+	Player* m_player = nullptr;//プレイヤー
+private:
+	Vector3 m_toCameraPos = Vector3::One;//カメラポス
+	SpringCamera  m_springCamera;
+
+
 
 };
 
