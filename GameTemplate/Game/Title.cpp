3b86e2487eed8de9783df28ae07a314d16f443a7
yamanakakousyou <CA01244030@st.kawahara.ac.jp>
2025-03-04 12:07:19 +0900

    TitleのCPP

--- GameTemplate/Game/Title.cpp
+++ GameTemplate/Game/Title.cpp
@@ -1,2 +1,28 @@
 #include "stdafx.h"
 #include "Title.h"
+#include "Game.h"
+
+Title::Title()
+{
+	//‰æ‘œ‚ğ“Ç‚İ‚Ş
+	spriteRender;
+}
+
+Title::~Title()
+{
+
+}
+
+void Title::Update()
+{
+	if (g_pad[0]->IsTrigger(enButtonA))
+	{
+
+	}
+}
+
+void Title::Render(RenderContext& rc)
+{
+	//‰æ‘œ‚ğ•`‰æ‚·‚éB
+	spriteRender.Draw(rc);
+}
