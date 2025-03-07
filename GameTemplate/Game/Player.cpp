f4533774a11037c46fd357f0d8b2af8283e96cb4
yamanakakousyou <CA01244030@st.kawahara.ac.jp>
2025-03-07 15:34:25 +0900

    Playerの変更

--- GameTemplate/Game/Player.cpp
+++ GameTemplate/Game/Player.cpp
@@ -2,9 +2,6 @@
 #include "Player.h"
 #include "Game.h"
 
-
-
-
 Player::Player()
 {
 
@@ -14,6 +11,64 @@ Player::~Player()
 {
 
 }
+
+void Player::Update() 
+{
+	Move();
+}
+
+void Player::Move()
+{
+	//xzの移動速度を0.0fにする。
+	moveSpeed.x = 0.0f;
+	moveSpeed.z = 0.0f;
+
+	//左スティックの入力量を取得。
+	Vector3 stickL;
+	stickL.x = g_pad[0]->GetLStickXF();
+	stickL.y = g_pad[0]->GetLStickYF();
+
+	//カメラの前方向と右方向のベクトルを持ってくる。
+	Vector3 forward = g_camera3D->GetForward();
+	Vector3 right = g_camera3D->GetRight();
+
+	//y方向には移動させない。
+	forward.y = 0.0f;
+	right.y = 0.0f;
+
+	//左スティックの入力量と500.0fを乗算。
+	right *= stickL.x * 500.0f;
+	forward *= stickL.y * 500.0f;
+
+	//移動速度にスティックの入力量を加算する。
+	moveSpeed += right + forward;
+
+	//地面に付いていたら。
+	if (characterController.IsOnGround() == true)
+	{
+		//重力を無くす。
+		moveSpeed.y = 0.0f;
+		//Aボタンが押されたら。
+		if (g_pad[0]->IsTrigger(enButtonA))
+		{
+			//ジャンプさせる。
+			moveSpeed.y = 240.0f;
+		}
+	}
+	//地面に付いていなかったら。
+	else
+	{
+		//重力を発生させる。
+		moveSpeed.y -= 5.5f;
+	}
+
+	//キャラクターコントローラーを使って座標を移動させる。
+	m_position = characterController.Execute(moveSpeed, 1.0f / 60.0f);
+
+	//絵描きさんに座標を教える。
+	modelRender.SetPosition(m_position);
+}
+
 
 bool Player::Start()
 {
--- GameTemplate/Game/Player.h
+++ GameTemplate/Game/Player.h
@@ -1,59 +1,32 @@
 #pragma once
 
-//ここから下クラス宣言
+
+//クラス宣言
 class Game;
 
-class Player
+
+class Player: public IGameObject
 {
 public:
-	//プレイや０のステート
+	//プレイヤーのステート
 	enum EnPlayerState {
 		//ここから下にステートを書く
 		enPlayerState_Idle,//待機
+
 
 	};
 	Player();
 	~Player();
 	bool Start();
 	void Update();
+	void Move();
 	void Render(RenderContext& rc);
-
-	//座標の設定
-	void SetPosition(const Vector3 position)
+	void SetPosition(const Vector3& position)
 	{
 		m_position = position;
 	}
 
-	//回転設定
-	void SetRotation(const Quaternion& rotation)
-	{
-		m_rotation = rotation;
-	}
-	
 
-
-
-
-	//ここから下プライベート(外部アクセスできないよ)
 private:
 
-	//移動処理
-	void Move();
-
-
-	//アニメーション
-	enum EnAnimationClip {
-
-		enAnimationClip_Idle,//待機アニメーション
-		enAnimationClip_Num,//アニメーション数
-	};
-
-	AnimationClip     m_animationClips[enAnimationClip_Num];//アニメーションクリップ
-	ModelRender       m_modelRender;                        //モデルレンダー
-	Vector3           m_position;                           //座標
-	Quaternion        m_rotation;                           //回転処理
-	Vector3           m_scale = Vector3::One;               //スケール
-
-	Game* m_game = nullptr;
 };
-
