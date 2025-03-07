45b10df7a15f5a7231b76aad31f9d1f992618739
yamanakakousyou <CA01244030@st.kawahara.ac.jp>
2025-03-07 12:04:16 +0900

    Timerの編集

--- GameTemplate/Game/Game.h
+++ GameTemplate/Game/Game.h
@@ -3,17 +3,19 @@
 #include "Level3DRender/LevelRender.h"
 
 class Player;
+class Timer;
 
 class Game : public IGameObject
 {
 public:
 	Game() {}
-	~Game() {}
+	~Game() ;
 	bool Start();
 	void Update();
 	void Render(RenderContext& rc);
 
 private:
+	Timer* m_timer;
 	ModelRender m_modelRender;
 	Vector3 m_pos;
 };
